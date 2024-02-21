0eb2575f9f5dcbf0bc67ad289806dcb9d5cf7778
Mandytrajano90 <mandytrajano90@gmail.com>
2024-02-21 16:14:49 -0300

    Integration test of GET /products

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,35 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
-describe('GET /products', function () { 
+describe('GET /products', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('should return a list of products', async function () {
+    const products = [
+      {
+        id: 1,
+        name: "Nemesis AR",
+        price: "10 v-bucks",
+        userId: 1,
+      },
+      {
+        id: 2,
+        name: "Hammer Pump Shotgun",
+        price: "20 v-bucks",
+        userId: 1,
+      },
+    ];
+    const productsStub = ProductModel.bulkBuild(products);
+    sinon.stub(ProductModel, 'findAll').resolves(productsStub);
+
+    const response = await chai.request(app).get('/products');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(products);
+  });
 });
