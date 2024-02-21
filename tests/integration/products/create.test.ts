54975983eb6a8201ab2d9ec32ead4375ef1baf63
Mandytrajano90 <mandytrajano90@gmail.com>
2024-02-21 15:08:45 -0300

    Integration test of /products route

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,21 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import app from '../../../src/app';
+import { newProduct, successfullyCreatedProduct } from '../../mocks/products.mock';
 
 chai.use(chaiHttp);
 
-describe('POST /products', function () { 
+describe('POST /products', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('posting a new product', async function () {
+    const body = newProduct;
+    const mockReturn = ProductModel.build(successfullyCreatedProduct);
+    sinon.stub(ProductModel, 'create').resolves(mockReturn);
+    const response = await chai.request(app).post('/products').send(body);
+    expect(response.status).to.equal(201);
+    expect(response.body).to.deep.equal(successfullyCreatedProduct);
+  });
 });
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,17 @@
+const newProduct = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  userId: 1
+}
+
+const successfullyCreatedProduct = {
+  id: 6,
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  userId: 1
+}
+
+export {
+  newProduct,
+  successfullyCreatedProduct,
+};
